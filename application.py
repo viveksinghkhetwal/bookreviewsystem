2e5258844eec4fbd130e68e29ba966c7dcd4720f
viveksinghkhetwal <48482586+viveksinghkhetwal@users.noreply.github.com>
2021-06-22 10:36:41 +0530

    sessions and pycache is added and files in static and templates are modified. application and import files are also updated.

--- application.py
+++ application.py
@@ -8,9 +8,6 @@ from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -18,9 +15,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgres://xcnprtumpcosiv:f433bdce0fdf582124511d6cb362aee4b0cb992e464c450e3a0149db02b12f79@ec2-52-0-155-79.compute-1.amazonaws.com:5432/dah2hrgpictefi")
 db = scoped_session(sessionmaker(bind=engine))
-
 
 @app.route("/")
 def index():
@@ -52,17 +48,44 @@ def login():
     pwd = request.form.get("pswd")
     if request.method == 'POST':
         if db.execute("SELECT * FROM userinfo WHERE email=:email AND pwd=:pwd",{"email":email,"pwd":pwd}).rowcount==0:
-            return render_template("error.html",message="Please, enter a valid email id and password.")
+            return render_template("error.html",message="Please, enter a valid email id and password. Kindly register if you are not registered.")
         if db.execute("SELECT * FROM userinfo WHERE email=:email AND pwd=:pwd",{"email":email,"pwd":pwd}).rowcount==1:
-            return redirect(url_for('account'))
-
+            email = db.execute("SELECT email from userinfo where email=:email",{"email":email}).fetchone()
+            session["id"]=email
+            #use email as a variable for userid.
+            return redirect(url_for('search'))
     return render_template('login.html')
 
-@app.route('/account', methods=['GET', 'POST'])
-def account():
-    textr = request.form.get("search")
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    s = request.form.get("search")
     if request.method == 'POST':
-        result = db.execute("SELECT * FROM books WHERE title like '%"+textr+"%' ORDER BY title").fetchall()
+        result = db.execute("SELECT * FROM books WHERE title like '%"+s+"%' or isbn like '%"+s+"%' or author like '%"+s+"%' ORDER BY title").fetchall()
         db.commit()
         return render_template("search.html", result=result)
     return render_template("account.html")
+
+
+@app.route('/search/book/<string:isbn>/<string:title>',methods=['GET','POST'])
+def book(title,isbn):
+    #Details of the a single book.
+    review = request.form.get("review")
+    check = db.execute("SELECT * from books where isbn=:id", {"id":isbn}).fetchone()
+    for i in session["id"]:
+        uid=i
+    if request.method=='POST':
+        if db.execute("SELECT * from reviews WHERE userid=:ui and bookid=:bi",{"ui":uid,"bi":check.isbn}).rowcount==0:
+            try:
+                db.execute("INSERT INTO reviews(reviews,userid,bookid) VALUES(:rv,:ui,:bi)",{"rv":review, "ui":uid, "bi":check.isbn})
+                db.commit()
+                return render_template("bookdetail.html")
+            except IntegrityError:
+                return render_template("error.html",message="You have submitted your review. User can review only once.")
+        if db.execute("SELECT * from reviews WHERE userid=:ui and bookid=:bi",{"ui":uid,"bi":check.isbn}).rowcount==1:
+            return render_template("error.html",message="User can review once.")
+    if check is None:
+        return render_template("error.html", message="No such book exists.")
+    userreviews = db.execute("SELECT * from reviews where bookid=:id", {"id":check.isbn}).fetchall()    #get book detail.
+    return render_template("bookdetail.html", check=check, userreviews=userreviews,title=title, isbn=isbn)
+    engine.dispose()
+
--- import.py
+++ import.py
@@ -3,21 +3,21 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgres://xcnprtumpcosiv:f433bdce0fdf582124511d6cb362aee4b0cb992e464c450e3a0149db02b12f79@ec2-52-0-155-79.compute-1.amazonaws.com:5432/dah2hrgpictefi")
 db = scoped_session(sessionmaker(bind=engine))
 
 def usertable():
-    db.execute("CREATE TABLE userinfo( fname varchar(70) NOT NULL, email varchar(150) PRIMARY KEY NOT NULL, pwd VARCHAR(50) NOT NULL);")
+    db.execute("CREATE TABLE userinfo(id SERIAL PRIMARY KEY, fname varchar(70) NOT NULL, email varchar(100) NOT NULL UNIQUE, pwd VARCHAR(50) NOT NULL);")
     print(f"table to store userinfo is created.")
     db.commit()
 
 def bookstable():
-    db.execute("CREATE TABLE books( isbn_no varchar primary key NOT NULL, title varchar(200) NOT NULL, author VARCHAR(80) NOT NULL, pbyear int not null );")
+    db.execute("CREATE TABLE books(id SERIAL primary key, isbn_no varchar NOT NULL UNIQUE, title varchar(200) NOT NULL, author VARCHAR(80) NOT NULL, pbyear int not null );")
     print(f"table book's storage is created")
     db.commit()
 
 def reviewtable():
-    db.execute("CREATE TABLE reviews(isbn_no varchar not null, email varchar(150) not null, rating int, reviews varchar(700));")
+    db.execute("CREATE TABLE reviews(id SERIAL primary key, rating int, reviews varchar(900) not null,isbn_no varchar not null, email varchar(150) not null);")
     print(f"table for the reviews is created.")
     db.commit()
 
